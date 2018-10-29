59aad8a209d05a311761d958b6eee147f8e81d98
safir888 <safiranggerds@gmail.com>
2018-10-29 08:03:20 +0700

    upload pertama

---
+++ src/snapapp10/SnapAPP10.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snapapp10;
+
+/**
+ *
+ * @author User ID
+ */
+public class SnapAPP10 extends SnapFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          new SnapFrame().setVisible(true);
+        // SnapAPP10 snap = new SnapAPP10();
+        //snap.setVisible(true);
+    }
+    
+}
